8b3e8fae4c9cc6eb38d0db53b844ca7365e85a82
Isaac Caparros <isaac.caparros@wiris.com>
2022-08-11 00:49:13 +0200

    fix: algosdk config

--- src/lib/algorand/index.ts
+++ src/lib/algorand/index.ts
@@ -1,11 +1,16 @@
 import algosdk from 'algosdk';
 import config from 'config';
 
+// Uncomment this if using Purestake
+/*
 const token = {
   'x-api-key': config.get('algorand.token') as string,
 };
-const port = config.get('algorand.server') as string;
-const server = config.get('algorand.port') as string;
+*/
+//Comment next line if using Purestake
+const token = config.get('algorand.token');
+const port = config.get('algorand.port') as string;
+const server = config.get('algorand.server') as string;
 export const algodClient: algosdk.Algodv2 = new algosdk.Algodv2(
   token,
   server,
